cd8c0877da465c26df82093f2d9ae52abc64f0ea
filmmaker.og <thefilmmaker.og@gmail.com>
2026-02-06 02:33:16 -0600

    feat: add routes for capital, fees, and waterfall info pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import IntroView from "./pages/IntroView";
 import BudgetInfo from "./pages/BudgetInfo";
+import CapitalInfo from "./pages/CapitalInfo";
+import FeesInfo from "./pages/FeesInfo";
+import WaterfallInfo from "./pages/WaterfallInfo";
 import Auth from "./pages/Auth";
 import Calculator from "./pages/Calculator";
 import Store from "./pages/Store";
@@ -23,6 +26,9 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/intro" element={<IntroView />} />
           <Route path="/budget-info" element={<BudgetInfo />} />
+          <Route path="/capital-info" element={<CapitalInfo />} />
+          <Route path="/fees-info" element={<FeesInfo />} />
+          <Route path="/waterfall-info" element={<WaterfallInfo />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/calculator" element={<Calculator />} />
           <Route path="/store" element={<Store />} />
